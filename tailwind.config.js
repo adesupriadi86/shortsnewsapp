2b6daf3658aecb05502c428b5a308d4d7715d326
adesupriadi86 <oketutor7@gmail.com>
2025-12-06 15:13:14 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./index.tsx",
+    "./App.tsx",
+    "./components/**/*.{ts,tsx}",
+    "./services/**/*.{ts,tsx}",
+    "./utils/**/*.{ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          750: "#2d3748",
+          850: "#1a202c",
+          950: "#0d1117",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
